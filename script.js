73384a7c23eb6b4e4f55030898acdb1e6d400233
mpatsche <42234573+mpatsche@users.noreply.github.com>
2021-03-22 07:00:02 +0100

    files added

---
+++ script.js
@@ -0,0 +1,33 @@
+/**
+ * 01: Assignment
+ * 
+ * Create a new JSON file in this project.
+ * Name the file: students.json
+ * In the JSON file create an array called students.
+ * In the array: Add an object for each student 
+ * in your class with the name of the student and 
+ * the zip code of each student's home address.
+ * 
+ * Validate the JSON in https://jsonlint.com/  
+ */
+
+
+
+/**
+ * 02: Assignment
+ * 
+ * Create a new XMLHttpRequest where you get the 
+ * data from the JSON file in assignment 1.
+ * Write a script which adds the names of the 
+ * students in the <ul> by creating new <li>s
+ * and adding them to the ul when the user clicks
+ * on the button ('View the students').
+ */
+
+
+
+
+/**
+ * Answer to assignments are here:
+ * http://malenekjeldsen.dk/2sem/10-JS-assignments/
+ */
